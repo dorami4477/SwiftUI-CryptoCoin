8ca9c5700c122ee58a25048e23340f6461d62a5e
dahyun <dahyun4477@gmail.com>
2024-09-06 14:41:45 +0900

    Feat: 코인 트렌드 페이지 UI 세팅

--- SwiftUI-CryptoCoin/ContentView.swift
+++ SwiftUI-CryptoCoin/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rows = [ GridItem(.fixed(80)), GridItem(.fixed(80))]
+    let colors: [Color] = [.black, .blue, .brown, .cyan, .gray, .indigo, .mint, .yellow, .orange, .purple]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("My Favorite")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                ScrollView(.horizontal) {
+                    LazyHStack{
+                        ForEach(0..<5) { item in
+                            favoriteList()
+                        }
+                    }
+                }
+                Text("Top 15 Coin")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                ScrollView(.horizontal) {
+                      LazyHGrid(rows: rows) {
+                        ForEach(colors, id: \.self) { color in
+                          RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 300, height: 80)
+                            .foregroundColor(color)
+                        }
+                      }
+                    }
+            }
+            .padding()
+            .navigationTitle("Crypto Coin")
         }
-        .padding()
+    }
+    
+    func favoriteList() -> some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.gray.opacity(0.2))
+                .frame(width: 220, height: 160)
+            VStack{
+                HStack{
+                    Image(systemName: "heart")
+                        .resizable()
+                        .scaledToFit()
+                        .background(.yellow)
+                        .clipShape(Circle())
+                        .frame(width: 40, height: 40)
+                        
+                    VStack(alignment: .leading) {
+                        Text("Bitcoin")
+                        Text("BTC")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                Text("69,234,245")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text("+0.64%")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding()
+            .frame(width: 220, height: 160)
+        }
     }
 }
 
