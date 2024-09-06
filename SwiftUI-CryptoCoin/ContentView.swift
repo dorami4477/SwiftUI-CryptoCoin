4afeaae9ff6ff2f9ba499d3d704ce02f2ea92b42
dahyun <dahyun4477@gmail.com>
2024-09-06 11:41:24 +0900

    Initial Commit

---
+++ SwiftUI-CryptoCoin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-CryptoCoin
+//
+//  Created by 박다현 on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-CryptoCoin/SwiftUI_CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoCoinApp.swift
+//  SwiftUI-CryptoCoin
+//
+//  Created by 박다현 on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
