b554b3bdb6a0aef2a09ca1c452becc85b5dbe53d
dahyun <dahyun4477@gmail.com>
2024-09-08 00:24:33 +0900

    Feat: SearchView UI 그리기

--- SwiftUI-CryptoCoin/SearchView.swift
+++ SwiftUI-CryptoCoin/SearchView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView{
+                LazyVStack{
+                    ForEach(0..<10) { item in
+                        searchResult()
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .navigationTitle("Search")
+            .searchable(text: $searchText)
+            .toolbar {
+                Button {
+                    print("버튼클릭")
+                } label: {
+                    Image(systemName: "star")
+                }
+                
+            }
+        }
+
+    }
+    
+    func searchResult() -> some View {
+        HStack{
+            Image(systemName: "star")
+            VStack{
+                Text("Hello, World!")
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            Spacer()
+            Image(systemName: "star")
+        }
+        .padding(.vertical)
     }
 }
 
